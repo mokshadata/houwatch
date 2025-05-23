4918156be8e8d5ef395314f1820780a3f29eef1d
Amanda Shih <pandafulmanda@gmail.com>
2025-05-23 16:35:16 -0500

    fix base of ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 import Pages from 'vite-plugin-pages';
 
 export default defineConfig({
+  base: "/houwatch/",
   plugins: [
     Pages({
       dirs: ['src/pages'],
